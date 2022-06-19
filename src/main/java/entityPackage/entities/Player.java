d1a7b99d395a425faf8d95f732088241078375a2
BaffIrodov <sergej.davidyuk@yandex.ru>
2022-06-19 15:45:45 +0300

    добавлены классы игрока и команды

---
+++ src/main/java/entityPackage/entities/Player.java
@@ -0,0 +1,8 @@
+package entityPackage.entities;
+
+public class Player {
+    public int id; //идентификатор игрока
+    public float kd; //отношение киллов к смертям (от 0,1 до 2)
+    public float adr; //количество урона за раунд (от 40 до 120)
+    public float stability; //от 0,6 до 1 (но как бы проценты)
+}
---
+++ src/main/java/entityPackage/entities/Team.java
@@ -0,0 +1,7 @@
+package entityPackage.entities;
+
+import java.util.List;
+
+public class Team {
+    public List<Player> listPlayer;
+}
