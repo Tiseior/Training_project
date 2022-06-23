e702b9991e6beb83b6a88876d40106b7ae798880
Ilya <sir.trubin000@yandex.ru>
2022-06-23 18:22:15 +0300

    Реализовано выявление команды-победителя, основываясь на силе команд.

--- src/main/java/entityPackage/MyJava2.java
+++ src/main/java/entityPackage/MyJava2.java
@@ -37,13 +37,14 @@ public class MyJava2 {
         System.out.println("Размер листа в Str: " + plsStr.size());
         pl = null;
 
-        Team t1 = new Team(plsFor);
-        Team t2 = new Team(plsRec);
-        Team t3 = new Team(plsStr);
+        Team t1 = new Team(plsFor, 1);
+        Team t2 = new Team(plsRec, 2);
+        Team t3 = new Team(plsStr, 3);
 
         // ========================================================
-        int pls = 20; // Количество игроков
-        int tms = 4; // Количество команд
+        int pls = 2000; // Количество игроков
+        int tms = 400; // Количество команд                                     // !!!!!
+
         List<Player> playersList = new ArrayList<>();
         for (int i = 1; i <= pls; i++) {
             Random rand = new Random();
@@ -51,24 +52,23 @@ public class MyJava2 {
         }
 
         // Понятное дело, что я это не использовал для 2000 игроков
-        System.out.println("Посмотрим, что вышло:\n");
+        /*System.out.println("Характеристики игроков:\n");
         for (Player l : playersList) {
             l.infoPlayer();
-        }
+        }*/
 
         // Разбиение на команды, используя subList, где j - это количество человек в команде
         List<Team> teamsList = new ArrayList<>();
         Collections.shuffle(playersList);
-        for (int i = 0, j = pls / tms; i < pls; i += j) {
-            teamsList.add(new Team(playersList.subList(i, i + j)));
+        for (int i = 0, j = pls / tms, ind = 1; i < pls; i += j, ind++) {
+            teamsList.add(new Team(playersList.subList(i, i + j), ind));
         }
 
-        System.out.println("Составы команд:");
+        /*System.out.println("Составы команд:");
         for (int i = 0; i < teamsList.size(); i++) {
-            System.out.print((i + 1) + ". ");
             teamsList.get(i).infoTeamId();
             System.out.println();
-        }
+        }*/
 
         // Тест нового класса с созданием игроков
         CreatePlayers lst = new CreatePlayers();
@@ -81,16 +81,52 @@ public class MyJava2 {
         //plsRand.infoPlayer();
         // =======================================
 
-        System.out.println("\nРассчитаем силу игроков на данный момент (с учётом стабильности): ");
+        /*System.out.println("\nРассчитаем силу игроков на данный момент (с учётом стабильности): ");
         for (Player l : playersList) {
             System.out.println("Сила игрока " + l.id + " = " + l.playerPower());
         }
 
         System.out.println("\nРассчитаем силу команд на данный момент (с учётом стабильности): ");
         for (int i = 0; i < teamsList.size(); i++) {
-            System.out.print((i + 1) + ". " + teamsList.get(i).teamPower());
+            System.out.print(teamsList.get(i).id + ". " + teamsList.get(i).teamPower());
             System.out.println();
+        }*/
+
+        // =============================================================================
+        Collections.shuffle(teamsList);
+        System.out.print("\nСписок команд: ");
+        int mx = 0;
+        for (int i = 0; i < teamsList.size(); i++) {
+            System.out.print(teamsList.get(i).id + " ");
+            if (teamsList.get(i).teamPowerNoStability() > teamsList.get(mx).teamPowerNoStability()) {
+                mx = i;
+            }
         }
+        System.out.println("\nПредполагаемый победитель - команда " + teamsList.get(mx).id);
+        System.out.println("\n\tНачинаем игру\n");
+        while (teamsList.size() > 1) {
+            float pw1 = teamsList.get(0).teamPower();
+            float pw2 = teamsList.get(1).teamPower();
+            System.out.println("Команда " + teamsList.get(0).id + " (Сила " + pw1 + ") vs Команда "
+                               + teamsList.get(1).id + " (Сила " + pw2 + ")");
+            if (pw1 > pw2) {
+                System.out.println("Победила команда " + teamsList.get(0).id);
+                teamsList.add(teamsList.get(0));
+            } else {
+                System.out.println("Победила команда " + teamsList.get(1).id);
+                teamsList.add(teamsList.get(1));
+            }
+            teamsList.remove(0);
+            teamsList.remove(0);
+            System.out.println("");
+        }
+        System.out.println("Победитель - команда " + teamsList.get(0).id);
+
+        // Влияние стабильности игроков на силу команды
+        /*System.out.println("Исследуется команда победитель");
+        for (int i = 1; i <= 20; i++) {
+            System.out.println("Тест " + i + ", сила: " + teamsList.get(0).teamPower());
+        }*/
     }
 
     public static List<Player> recFunc(List<Player> lst, int i) {
--- src/main/java/entityPackage/entities/Team.java
+++ src/main/java/entityPackage/entities/Team.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 public class Team {
     public List<Player> listPlayer;
+    public int id;
 
-    public Team(List<Player> listPlayer) {
+    public Team(List<Player> listPlayer, int id) {
         this.listPlayer = listPlayer;
+        this.id = id;
     }
 
     public float teamPower() {
@@ -17,7 +19,18 @@ public class Team {
         return pw;
     }
 
+    public float teamPowerNoStability() {
+        float pw = 0;
+        final float AVG_KD = 0.95f;
+        final float AVG_ADR = 74;
+        for (int i = 0; i < listPlayer.size(); i++) {
+            pw += (listPlayer.get(i).kd / AVG_KD * 50) + (listPlayer.get(i).adr / AVG_ADR * 50);
+        }
+        return pw;
+    }
+
     public void infoTeamId() {
+        System.out.print("Команда " + id + "\n\t");
         for (int i = 0; i < listPlayer.size(); i++) {
             System.out.print(listPlayer.get(i).id + " ");
         }
